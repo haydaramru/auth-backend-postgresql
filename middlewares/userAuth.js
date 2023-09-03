28f2590107a3754b12e095275766166c26e1a466
Haydar <haydaramrurevanda@mail.ugm.ac.id>
2023-09-03 21:44:01 +0700

    modified models routes

--- middlewares/userAuth.js
+++ middlewares/userAuth.js
@@ -1,6 +1,7 @@
 //importing modules
 const express = require("express");
-const db = require("../Models");
+const db = require("../models");
+
 //Assigning db.users to User variable
  const User = db.users;
 
